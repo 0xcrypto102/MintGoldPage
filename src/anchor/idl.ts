fe63e0e1cea0818488025e8c039b6932d0147e66
BC.Crypto <78132698+0xcrypto102@users.noreply.github.com>
2024-06-15 00:59:55 +0300

    Update idl.ts

--- src/anchor/idl.ts
+++ src/anchor/idl.ts
@@ -1,5 +1,5 @@
 export type Dapp = {
-  "address": "E7GAterNGHfNpn4p9kEgWeNvhKMYbkTsgukBUNFEv52p",
+  "address": "3J51b7AMJZxmenuKynRevytgmPRFF93BPZcRSH6VT6xV",
   "metadata": {
     "name": "dapp",
     "version": "0.1.0",
@@ -1066,7 +1066,7 @@ export type Dapp = {
 };
 
 export const IDL: Dapp = {
-  "address": "E7GAterNGHfNpn4p9kEgWeNvhKMYbkTsgukBUNFEv52p",
+  "address": "3J51b7AMJZxmenuKynRevytgmPRFF93BPZcRSH6VT6xV",
   "metadata": {
     "name": "dapp",
     "version": "0.1.0",
